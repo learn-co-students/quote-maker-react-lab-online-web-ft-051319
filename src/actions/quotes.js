df349a46ec2acdc6a880beec6a81a76e2f58fc98
Brian Velez <bvez94@gmail.com>
2019-10-01 20:14:19 -0700

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,30 @@
 // TODO: Create action creators as defined in tests
+
+const addQuote = quote => ({
+    type: "ADD_QUOTE",
+    quote: {...quote, votes: 0}
+    });
+
+const removeQuote = quoteId => ({
+    type: "REMOVE_QUOTE",
+    quoteId: quoteId
+    });
+
+const upvoteQuote = quoteId => ({
+    type: "UPVOTE_QUOTE",
+    quoteId: quoteId
+    });
+
+const downvoteQuote = quoteId => ({
+    type: "DOWNVOTE_QUOTE",
+    quoteId: quoteId
+    });
+
+export {addQuote, removeQuote, upvoteQuote, downvoteQuote};
+
+// export function addQuote(quote) {
+//     return {
+//     type: 'ADD_QUOTE',
+//     quote: {...quote}
+//     };
+// };
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,27 @@
 export default (state = [], action) => {
-  return state;
+  switch(action.type) {
+    case 'ADD_QUOTE':
+      state.push(action.quote);
+    case 'REMOVE_QUOTE':
+      state = state.filter(quote => quote.id !== action.quoteId);
+      // return state;
+    case 'UPVOTE_QUOTE':
+      state = state.map(quote => {
+        if (quote.id === action.quoteId) {
+          quote.votes += 1;
+        }
+        return quote
+      });
+      return state 
+    case 'DOWNVOTE_QUOTE':
+      state = state.map(quote => {
+        if (quote.id === action.quoteId && quote.votes > 0) {
+          quote.votes -= 1;
+        }
+        return quote
+      });
+      return state
+    default: 
+      return state 
+  };
 }
