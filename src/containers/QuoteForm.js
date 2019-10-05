c9d90aa344f85e049bd04f7afa704fa37b56ce5b
github username <github email address>
2019-10-05 15:04:25 -0500

    Done.

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -6,20 +6,33 @@ import { addQuote } from '../actions/quotes';
 class QuoteForm extends Component {
 
   state = {
+    content: '',
+    author: ''
     //set up a controlled form with internal state
   }
 
   handleOnChange = event => {
-    // Handle Updating Component State
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
   }
+
 
   handleOnSubmit = event => {
+    event.preventDefault()
     // Handle Form Submit event default
     // Create quote object from state
+    const quote = { ...this.state, id: uuid() }
     // Pass quote object to action creator
+    this.props.addQuote(quote)
     // Update component state to return to default state
+    this.setState({
+      content: '',
+      author: ''
+    })
   }
-
+ 
   render() {
     return (
       <div className="container">
@@ -27,13 +40,16 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea
                         className="form-control"
                         value={this.state.content}
+                        onChange={this.handleOnChange}
+                        name="content"
+                    
                       />
                     </div>
                   </div>
@@ -44,6 +60,8 @@ class QuoteForm extends Component {
                         className="form-control"
                         type="text"
                         value={this.state.author}
+                        name="author"
+                        onChange={this.handleOnChange}
                       />
                     </div>
                   </div>
@@ -63,4 +81,4 @@ class QuoteForm extends Component {
 }
 
 //add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(null, {addQuote})(QuoteForm);
--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
 
 class Quotes extends Component {
 
   render() {
+    const {quotes, removeQuote, upvoteQuote, downvoteQuote} = this.props//destructuring objects from redux store and passed below to the quotes.map
     return (
       <div>
         <hr />
@@ -15,18 +17,24 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              
+                {quotes.map(quote => <QuoteCard key={quote.id} quote={quote} upvoteQuote={upvoteQuote} downvoteQuote={downvoteQuote} removeQuote={removeQuote} />)}
+                {/* Passing props to QuoteCard component */}
+               {/* Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes*/}
+               
             </div>
           </div>
-        </div>
+        </div> 
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    quotes: state.quotes  //used above in the render function
+  }
+}
 //add arguments to connect as needed
-export default connect()(Quotes);
+
+export default connect(mapStateToProps, { removeQuote, upvoteQuote, downvoteQuote })(Quotes);
